1e713d7f2bd9dbe3081ccca730ea73570d9c12c7
luca <luca.bisognin13@gmail.com>
2025-12-10 19:09:39 +0100

    v 1.0.2 - lavoro 10/12/2025 - luca

--- core/src/main/sorgente/Lobby/LobbyManager.java
+++ core/src/main/sorgente/Lobby/LobbyManager.java
@@ -1,4 +1,39 @@
 package sorgente.Lobby;
 
-public class LobbyManager {
+import com.badlogic.gdx.ScreenAdapter;
+
+public class LobbyManager extends ScreenAdapter {
+    private LobbyUI ui;
+    private LobbyInput input;
+
+    // costruttore
+    LobbyManager() {
+        input = new LobbyInput();
+        ui = new LobbyUI();
+    }
+
+    // ====================== //
+    //   METODI DELLO SCREEN  //
+    // ====================== //
+
+    @Override
+    public void render(float delta) {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+    }
 }
