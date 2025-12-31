8e0fdd562517f45a3bf5cbd087dae589c382af8e
Lorenzo <lorenzoarevalo@outlook.it>
2025-12-31 11:39:26 +0100

    v 1.0.2 - lavoro 30/12/2025 - lorenzo

--- core/src/main/sorgente/Game/GameInput.java
+++ core/src/main/sorgente/Game/GameInput.java
@@ -4,10 +4,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -19,8 +20,23 @@ public class GameInput implements InputProcessor
 
     protected boolean isBtnExitClicked;
     protected boolean isBtnExitHover;
+    protected boolean isHole;
 
     protected final Rectangle exit;
+
+    // 🔷 Griglia dei buchi
+    public Rectangle[][] holes = new Rectangle[6][7];
+    public Array<Rectangle> allHoles = new Array<>();
+
+    // 🔷 Dimensioni e offset corretti
+    private final float cellWidth  = 80f;
+    private final float cellHeight = 700f / 6f;
+    private final float holeSize   = 52f;
+    private final float offsetX    = (cellWidth - holeSize) / 2f; // ≈ 14
+    private final float offsetY    = (cellHeight - holeSize) / 2f;
+    private final float gridOffsetX = (1000f - (cellWidth * 7)) / 2f; // ≈ 220
+
+
 
     public GameInput()
     {
@@ -28,76 +44,105 @@ public class GameInput implements InputProcessor
         cursor = Gdx.graphics.newCursor(mouse, 0, 0);
         Gdx.graphics.setCursor(cursor);
 
-        exit=new Rectangle(100,199,52,52);
+        exit = new Rectangle(840, 93, 52, 52);
 
+        // 🔷 Genera tutti i buchi
+        for (int row = 0; row < 6; row++)
+        {
+            for (int col = 0; col < 7; col++)
+            {
+                float x = gridOffsetX + col * cellWidth + offsetX;
+                float y = 700f - ((row + 1) * cellHeight) + offsetY;
+
+                Rectangle rect = new Rectangle(x, y, holeSize, holeSize);
+                holes[row][col] = rect;
+                allHoles.add(rect);
+            }
+        }
     }
 
     @Override
-    public boolean keyDown(int i) {
-        return false;
-    }
+    public boolean touchDown(int x, int y, int pointer, int button)
+    {
 
-    @Override
-    public boolean keyUp(int i) {
-        return false;
-    }
+        log.info("Touch at: " + x + ", " + y);
+        checkHitBox(x, y);
 
-    @Override
-    public boolean keyTyped(char c) {
+        // 🔷 Rileva colonna cliccata
+        int col = getColumnFromClick(x, y);
+
+        if (col != -1)
+        {
+            log.info("Hai cliccato la colonna " + col);
+            isHole=true;
+        }
+
         return false;
     }
 
     private void checkHitBox(int x, int y)
     {
-      if(exit.contains(x,y))
-      {
-        isBtnExitClicked=true;
-        new Timer().schedule(new TimerTask() { @Override public void run() { isBtnExitClicked = false; } }, 100);
-      }
+        if (exit.contains(x, y))
+        {
+            log.info("Hai cliccato il bottone di uscita");
+            isBtnExitClicked = true;
+
+            new Timer().schedule(new TimerTask() {
+                @Override public void run() {
+                    isBtnExitClicked = false;
+                }
+            }, 100);
+        }
     }
 
     @Override
-    public boolean touchDown(int x, int y, int i2, int i3)
+    public boolean mouseMoved(int x, int y)
     {
-
-        log.info(x+" "+y);
-
-        checkHitBox(x,y);
-
+        isBtnExitHover = exit.contains(x, y);
         return false;
     }
 
-    private void resetHover()
+
+    public int getColumnFromClick(int x, int y)
     {
+        for (int col = 0; col < 7; col++) {
+            Rectangle topCell = holes[0][col]; // tutte le celle della colonna hanno la stessa x
+            float colX = topCell.x;
+            float colWidth = topCell.width;
 
+            if (x >= colX && x <= colX + colWidth)
+            {
+                return col;
+            }
+        }
+        return -1;
     }
 
-    @Override
-    public boolean touchUp(int i, int i1, int i2, int i3) {
-        return false;
+
+
+
+    // 🔷 Ritorna la prima riga libera in una colonna (dal basso)
+    public int getLowestFreeRow(int col, boolean[][] board)
+    {
+        for (int row = 5; row >= 0; row--)
+        {
+            if (!board[row][col])
+            {
+                return row;
+            }
+        }
+        return -1; // colonna piena
     }
 
-    @Override
-    public boolean touchCancelled(int i, int i1, int i2, int i3) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int i, int i1, int i2) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int i, int i1) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float v, float v1) {
-        return false;
-    }
+    @Override public boolean keyDown(int i) { return false; }
+    @Override public boolean keyUp(int i) { return false; }
+    @Override public boolean keyTyped(char c) { return false; }
+    @Override public boolean touchUp(int i, int i1, int i2, int i3) { return false; }
+    @Override public boolean touchCancelled(int i, int i1, int i2, int i3) { return false; }
+    @Override public boolean touchDragged(int i, int i1, int i2) { return false; }
+    @Override public boolean scrolled(float v, float v1) { return false; }
 
     public void dispose() {
-
+        mouse.dispose();
     }
 }
--- core/src/main/sorgente/Game/GameUI.java
+++ core/src/main/sorgente/Game/GameUI.java
@@ -6,17 +6,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import org.w3c.dom.Text;
+import com.badlogic.gdx.math.Rectangle;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import sorgente.Fonts;
-import sorgente.LoadingScreen;
-import sorgente.Lobby.LobbyInput;
+import sorgente.Lobby.LobbyManager;
 import sorgente.Main;
 import sorgente.ResourceLoader;
 
-public class GameUI extends ScreenAdapter implements ResourceLoader
-{
+public class GameUI extends ScreenAdapter implements ResourceLoader {
+    private static final Log log = LogFactory.getLog(GameUI.class);
     private final Main game;
-    private  final SpriteBatch screen;
+    private final SpriteBatch screen;
 
     private final GlyphLayout layout = new GlyphLayout();
     private float cursorTimer = 0f;
@@ -28,78 +29,169 @@ public class GameUI extends ScreenAdapter implements ResourceLoader
 
     private boolean darkMode;
 
-    private Texture forza;
+    private float modeTransitionTimer = 0f;
+    private boolean modeTransition = false;
+    private int pendingMode = -1;
 
+    // Light Texture
+    private Texture lightForza;
+    private Texture lightExit;
+    private Texture lightExitHover;
+
+    // Dark Texture
+    private Texture darkForza;
+    private Texture darkExit;
+    private Texture darkExitHover;
+
+    // Editable Texture
+    private Texture forza;
     private Texture exit;
     private Texture exitHover;
 
+    private Texture red;
+    private Texture yellow;
 
+    // Stato della griglia: 0 = vuoto, 1 = rosso
+    private int[][] boardState = new int[6][7];
 
-
-    public GameUI(Main game, boolean dark)
-    {
+    public GameUI(Main game, boolean dark) {
         this.game = game;
         this.screen = game.screen;
         Fonts.load();
         shapeRenderer = new ShapeRenderer();
-
-        darkMode=dark;
-
-
+        darkMode = dark;
         this.loadImages();
 
+        // Inizializza boardState
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 7; c++) {
+                boardState[r][c] = 0;
+            }
+        }
     }
 
-    private void draw(Texture texture, boolean response, float x, float y)
-    {
-        if (response)
-        {
+    private void draw(Texture texture, boolean response, float x, float y) {
+        if (response) {
             screen.draw(texture, x, y);
         }
     }
 
+    private void loadDarkMode() {
+        lightForza = new Texture("game_mods_screens/base_light.png");
+        lightExit = new Texture("ui/buttons/lobby/light/btn_close.png");
+        lightExitHover = new Texture("ui/buttons/lobby/light/btn_close_clicked.png");
+    }
+
+    private void loadLightMode() {
+        darkForza = new Texture("game_mods_screens/base_dark.png");
+        darkExit = new Texture("ui/buttons/lobby/dark/btn_close.png");
+        darkExitHover = new Texture("ui/buttons/lobby/dark/btn_close_clicked.png");
+    }
+
+    private void isDark(boolean dark) {
+        if (dark) {
+            forza = darkForza;
+            exit = darkExit;
+            exitHover = darkExitHover;
+        } else {
+            forza = lightForza;
+            exit = lightExit;
+            exitHover = lightExitHover;
+        }
+    }
+
+    private boolean[][] boardStateAsBoolean() {
+        boolean[][] b = new boolean[6][7];
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 7; c++) {
+                b[r][c] = boardState[r][c] != 0;
+            }
+        }
+        return b;
+    }
+
     @Override
-    public void render(float delta)
-    {
+    public void render(float delta) {
         Gdx.input.setInputProcessor(gameInput);
         screen.begin();
 
-        screen.draw(forza,0,0);
+        isDark(darkMode);
+        screen.draw(forza, 0, 0);
 
+        draw(exit, gameInput.isBtnExitClicked, 840, 600);
+        draw(exitHover, gameInput.isBtnExitHover, 840, 600);
 
-           draw(exit, gameInput.isBtnExitClicked, 122,122);
+        float baseX = 269;
+        float baseY = 130;
+
+// 🔴 Disegna le pedine rosse
+        for (int row = 5; row >= 0; row--)
+        {
+            float drawY = baseY + (5 - row) * 64;
+            float drawX = baseX;
+
+            for (int col = 0; col < 7; col++)
+            {
+                if (boardState[row][col] == 1)
+                {
+                    draw(red, true, drawX, drawY);
+                }
+
+                drawX += 71;
+            }
+        }
 
 
         screen.end();
+
+        if (gameInput.isHole) {
+            int col = gameInput.getColumnFromClick(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+
+            if (col != -1) {
+                int row = gameInput.getLowestFreeRow(col, boardStateAsBoolean());
+
+                if (row != -1) {
+                    boardState[row][col] = 1; // 1 = rosso
+                } else {
+                    log.warn("Colonna " + col + " piena, impossibile inserire pedina.");
+                }
+
+                gameInput.isHole = false; // resetta sempre il flag
+            }
+        }
+
+
+        if (gameInput.isBtnExitClicked) {
+            modeTransition = true;
+            pendingMode = 0;
+        }
+
+        if (modeTransition) {
+            modeTransitionTimer += delta;
+            if (modeTransitionTimer >= 0.14f) {
+                if (pendingMode == 0) {
+                    game.setScreen(new LobbyManager(game));
+                    return;
+                }
+            }
+        }
+
+
     }
 
     @Override
-    public void loadFont() {
+    public void loadFont() {}
 
+    @Override
+    public void loadImages() {
+        loadDarkMode();
+        loadLightMode();
+        red = new Texture("ui/icons/red.png");
+        yellow = new Texture("ui/icons/yellow.png");
     }
 
     @Override
-    public void loadImages()
-    {
-
-       if(darkMode)
-       {
-           forza = new Texture("game_mods_screens/base_dark.png");
-           exit= new Texture("ui/buttons/lobby/dark/btn_close.png");
-           exitHover=new Texture("ui/buttons/lobby/light/btn_close.png");
-       }
-       else
-       {
-           forza = new Texture("game_mods_screens/base_light.png");
-           exit= new Texture("ui/buttons/lobby/light/btn_close.png");
-           exitHover=new Texture("ui/buttons/lobby/light/btn_close.png");
-       }
-    }
-
-    @Override
-    public void dispose()
-    {
+    public void dispose() {
         forza.dispose();
-
     }
 }
